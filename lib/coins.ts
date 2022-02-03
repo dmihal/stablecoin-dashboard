e6c60b20a7ec2b706c108441e0f4d69337d5a3b7
David Mihal <david@dmihal.com>
2022-02-03 17:49:46 +0000

    Implement dashboard

---
+++ lib/coins.ts
@@ -0,0 +1,98 @@
+function _formatInteger(int: number) {
+  return Math.floor(int)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function _formatMoney(amount: number, denom = "", decPlaces = 2) {
+  const dec = amount % 1;
+  return (
+    denom +
+    _formatInteger(amount) +
+    (decPlaces > 0 ? dec.toFixed(decPlaces).slice(1) : "")
+  );
+}
+
+function _calculateAvgDev(numbers: number[], _mean?: number) {
+  const mean = _mean ? _mean : numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  return Math.sqrt(
+    numbers.map((x) => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / numbers.length
+  );
+}
+
+function _calculateMaxDev(numbers: number[], _mean?: number) {
+  const mean = _mean ? _mean : numbers.reduce((a, b) => a + b, 0) / numbers.length;
+  return Math.sqrt(Math.max(...numbers.map((x) => Math.pow(x - mean, 2))));
+}
+
+export enum CoinType {
+  Centralized,
+  Decentralized,
+  Mixed,
+  Algorithmic,
+  Unknown,
+}
+
+export class Coin {
+  name: string;
+  coingeckoId: string;
+  type: CoinType;
+  icon: string | undefined;
+  targetPrice: number = 1;
+  currentPrice: number | undefined = undefined;
+  currentMarketCap: number | undefined = undefined;
+  historicalPrices: number[] | undefined = undefined;
+  avgDev: number | undefined = undefined;
+  maxDev: number | undefined = undefined;
+  stable: boolean = false;
+
+  constructor(name: string, coingeckoId: string, type: CoinType, icon?: string) {
+    this.name = name;
+    this.coingeckoId = coingeckoId;
+    this.type = type;
+    this.icon = icon;
+  }
+
+  async fetchData(days: string) {
+    console.log(`Attempting to fetch data for ${this.name}...`);
+
+    try {
+      const req = await fetch(`https://api.coingecko.com/api/v3/coins/${this.coingeckoId}/market_chart?vs_currency=usd&days=${days}`)
+      const data: {
+        prices: number[][];
+        market_caps: number[][];
+      } = await req.json();
+
+      data.prices.sort((a, b) => {
+        if (a[0] > b[0]) return -1;
+        else return 1;
+      });
+
+      data.market_caps.sort((a, b) => {
+        if (a[0] > b[0]) return -1;
+        else return 1;
+      });
+
+      this.currentPrice = data.prices[0][1];
+      this.currentMarketCap = data.market_caps[0][1];
+      this.historicalPrices = data.prices.map((x) => x[1]);
+
+      // // DEBUG MOD: just generate some random numbers
+      // this.currentPrice = Math.random();
+      // this.currentMarketCap = Math.random() * 1000000;
+      // this.historicalPrices = [Math.random(), Math.random(), Math.random()];
+
+      this.avgDev = _calculateAvgDev(this.historicalPrices, this.targetPrice);
+      this.maxDev = _calculateMaxDev(this.historicalPrices, this.targetPrice);
+
+      this.stable =
+        this.avgDev <= 0.005 * this.targetPrice &&
+        this.maxDev <= 0.015 * this.targetPrice;
+
+      console.log(`Successfully fetched data for ${this.name}!`);
+      // console.log(`Historical prices of ${this.name}`, this.historicalPrices);
+    } catch (e) {
+      console.log(`Failed to fetch data for ${this.name}! Error message: ${e}`);
+    }
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,331 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Coin, CoinType } from 'lib/coins'
+
+const COINS = [
+  new Coin("USDT", "tether", CoinType.Centralized, "images/usdt.png"),
+  new Coin("USDC", "usd-coin", CoinType.Centralized, "images/usdc.png"),
+  new Coin("BUSD", "binance-usd", CoinType.Centralized, "images/busd.png"),
+  new Coin("DAI", "dai", CoinType.Mixed, "images/dai.png"),
+  new Coin("UST", "terrausd", CoinType.Algorithmic, "images/ust.png"),
+  new Coin("TUSD", "true-usd", CoinType.Centralized, "images/tusd.png"),
+  new Coin("PAX", "paxos-standard", CoinType.Centralized, "images/pax.png"),
+  new Coin("LUSD", "liquity-usd", CoinType.Decentralized, "images/lusd.png"),
+  new Coin("USDN", "neutrino", CoinType.Algorithmic, "images/usdn.png"),
+  new Coin("HUSD", "husd", CoinType.Centralized, "images/husd.png"),
+  new Coin("FEI", "fei-protocol", CoinType.Algorithmic, "images/fei.png"),
+  new Coin("FRAX", "frax", CoinType.Mixed, "images/frax.png"),
+  new Coin("sUSD", "nusd", CoinType.Decentralized, "images/susd.png"),
+  new Coin("alUSD", "alchemix-usd", CoinType.Mixed, "images/alusd.png"),
+  new Coin("GUSD", "gemini-dollar", CoinType.Centralized, "images/gusd.png"),
+  new Coin("VAI", "vai", CoinType.Mixed, "images/vai.png"),
+  new Coin("USDX", "usdx", CoinType.Mixed, "images/usdx.png"),
+  new Coin("USDP", "usdp", CoinType.Mixed, "images/usdp.png"),
+  new Coin("CUSD", "celo-dollar", CoinType.Algorithmic, "images/cusd.png"),
+  new Coin("mUSD", "musd", CoinType.Mixed, "images/musd.png"),
+];
+
+function _formatInteger(int) {
+  return Math.floor(int)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function _formatMoney(amount, denom = "", decPlaces = 2) {
+  const dec = amount % 1;
+  return (
+    denom +
+    _formatInteger(amount) +
+    (decPlaces > 0 ? dec.toFixed(decPlaces).slice(1) : "")
+  );
+}
+
+function Tag({ type }) {
+  switch (type) {
+    case CoinType.Centralized:
+      return <span class="tag tag-centralized">centralized</span>;
+    case CoinType.Decentralized:
+      return <span class="tag tag-decentralized">decentralized</span>;
+    case CoinType.Mixed:
+      return <span class="tag tag-mixed">mixed</span>;
+    case CoinType.Algorithmic:
+      return <span class="tag tag-algorithmic">algorithmic</span>;
+    case CoinType.Unknown:
+      return <span class="tag tag-unknown">unknown</span>;
+  }
+}
 
 export default function Home() {
+  const [coins, setCoins] = useState(COINS)
+  const [percentage, setPercentage] = useState(0)
+  const [active, setActive] = useState(1)
+
+  const run = async (days) => {
+    console.log(`Fetching data for the last ${days} days...`);
+  
+    let count = 0;
+  
+    for (const coin of COINS) {
+      await coin.fetchData(days);
+  
+      count += 1;
+      const _percentage = ((100 * count) / COINS.length).toFixed(0);
+      setPercentage(_percentage)
+    }
+    setCoins(COINS)
+    setPercentage(0)
+  }
+
+  useEffect(() => {
+    typeof document !== undefined ? require('bootstrap/dist/js/bootstrap') : null
+
+    run(1)
+  }, [])
+
+  const sortByMarketCap = () => {
+    console.log("Sorting by market cap...");
+    
+    COINS.sort((a, b) => {
+      if (!a.currentMarketCap) return 1;
+      if (!b.currentMarketCap) return -1;
+      if (a.currentMarketCap > b.currentMarketCap) return -1;
+      else return 1;
+    });
+  
+    setCoins([...COINS])
+  };
+  
+  const sortByCurrentPrice = () => {
+    console.log("Sorting by current price...");
+    
+    COINS.sort((a, b) => {
+      if (!a.currentPrice) return 1;
+      if (!b.currentPrice) return -1;
+      if (a.currentPrice > b.currentPrice) return -1;
+      else return 1;
+    });
+  
+    setCoins([...COINS])
+  };
+  
+  const sortByAvgDev = () => {
+    console.log("Sorting by avg dev...");
+    
+    COINS.sort((a, b) => {
+      if (!a.avgDev) return -1;
+      if (!b.avgDev) return 1;
+      if (a.avgDev > b.avgDev) return 1;
+      else return -1;
+    });
+  
+    setCoins([...COINS])
+  };
+  
+  const sortByMaxDev = () => {
+    console.log("Sorting by max dev...");
+    
+    COINS.sort((a, b) => {
+      if (!a.maxDev) return -1;
+      if (!b.maxDev) return 1;
+      if (a.maxDev > b.maxDev) return 1;
+      else return -1;
+    });
+  
+    setCoins([...COINS])
+  };
+  
+  const sortByStable = () => {
+    console.log("Sorting by stable...");
+  
+    tbody.innerHTML = "";
+  
+    COINS.sort((a, b) => {
+      if (a.stable) return -1;
+      else if (b.stable) return 1;
+      else return 0;
+    });
+  
+    setCoins([...COINS])
+  };
+
+  const periodListener = days => () => {
+    setActive(days)
+    run(days)
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
+        <title>Stablecoins</title>
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Roboto&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+      {percentage > 0 && (
+        <div id="overlayContainer">
+          <div id="progress" class="progress w-100">
+            <div
+              id="progressBar"
+              class="progress-bar progress-bar-striped progress-bar-animated fs-3"
+              role="progressbar"
+              style={{ width: `${percentage}%` }}
+            ></div>
+          </div>
+          <div
+            id="overlayErrorMsg"
+            class="text-center text-danger fs-5 pt-3 pb-3 px-2 mt-3 bg-light d-none"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            If you request price data too frequently, CoinGecko may temporarily ban your IP
+            address. Wait a minute and try again.
+          </div>
         </div>
-      </main>
+      )}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div class="container mb-3">
+        <div class="title fs-1 me-3">Stablecoin Dashboard</div>
+      </div>
+
+      <div class="container mb-3">
+        <div role="group" class="btn-group">
+          <button type="button" class="btn btn-outline-primary" id="1d" onClick={periodListener(1)}>24h</button>
+          <button type="button" class="btn btn-outline-primary" id="7d" onClick={periodListener(7)}>7d</button>
+          <button type="button" class="btn btn-outline-primary" id="14d" onClick={periodListener(14)}>14d</button>
+          <button type="button" class="btn btn-outline-primary" id="30d" onClick={periodListener(30)}>30d</button>
+          <button type="button" class="btn btn-outline-primary" id="90d" onClick={periodListener(90)}>90d</button>
+          <button type="button" class="btn btn-outline-primary" id="180d" onClick={periodListener(180)}>180d</button>
+          <button type="button" class="btn btn-outline-primary" id="365d" onClick={periodListener(365)}>1y</button>
+        </div>
+      </div>
+      <div class="container mb-3">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">asset</th>
+              <th scope="col">type</th>
+              <th scope="col" id="sortByMarketCap">market cap ($)</th>
+              <th scope="col" id="sortByCurrentPrice">current price ($)</th>
+              <th scope="col" id="sortByAvgDev">avg deviation ($)</th>
+              <th scope="col" id="sortByMaxDev">max deviation ($)</th>
+              <th scope="col" id="sortByStable">stable</th>
+            </tr>
+          </thead>
+          <tbody id="tbody">
+            {COINS.map(coin => {
+              return (
+                <tr class="align-middle">
+                  <td>
+                    <div class="d-flex align-items-center">
+                      {coin.icon ? <img class="icon me-3" src={coin.icon} /> : ""}
+                      {coin.name}
+                    </div>
+                  </td>
+                  <td>
+                    <Tag type={coin.type} />
+                  </td>
+                  <td>{coin.currentMarketCap ? _formatMoney(coin.currentMarketCap, "", 0) : "?"}</td>
+                  <td>${coin.currentPrice ? coin.currentPrice.toPrecision(3) : "?"}</td>
+                  <td class={
+                    coin.avgDev && coin.avgDev <= 0.005 * coin.targetPrice ? "text-success" : ""
+                  }>
+                    {coin.avgDev ? coin.avgDev.toPrecision(3) : "?"}
+                  </td>
+                  <td class={
+                    coin.maxDev && coin.maxDev < 0.015 * coin.targetPrice ? "text-success" : ""
+                  }>
+                    {coin.maxDev ? coin.maxDev.toPrecision(3) : "?"}
+                  </td>
+                  <td>{coin.stable ? "✅" : ""}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+          <caption>
+            Click table headers to sort<br />
+            <i>Mixed</i>
+            means the stablecoin is partially collateralized by centralized assets<br />
+            <i>Stable</i>
+            is arbitrarily defined as having an average deviation of no more than 0.5 cent,
+            an a maximum deviation of no more than 1.5 cents, during the selected period<br />
+          </caption>
+        </table>
+      </div>
+
+      <style jsx>{`
+        body {
+          font-family: "Roboto", sans-serif;
+          padding-top: 1.5rem;
+          padding-bottom: 1.5rem;
+        }
+        td {
+          padding: 0.3rem 0.5rem !important;
+        }
+        .title {
+          font-family: "Dela Gothic One", cursive;
+        }
+        .icon {
+          width: 32px;
+          height: 32px;
+        }
+        .tag {
+          color: black;
+          border-radius: 5px;
+          padding: 6px 10px;
+        }
+        .tag-centralized {
+          background-color: #fd7e14;
+        }
+        .tag-mixed {
+          background-color: #ffc107;
+        }
+        .tag-decentralized {
+          background-color: #20c997;
+        }
+        .tag-algorithmic {
+          background-color: #0dcaf0;
+        }
+        .tag-unknown {
+          background-color: #adb5bd;
+        }
+        #overlayContainer {
+          display: block;
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          padding-top: 35vh;
+          padding-left: 5rem;
+          padding-right: 5rem;
+          background-color: rgba(245, 245, 245, 0.7);
+          z-index: 99999;
+          text-align: center;
+          transition: all 0.25s;
+          opacity: 1;
+        }
+        #progress {
+          height: 5rem;
+          box-shadow: rgba(0, 0, 0, 0.56) 0px 20px 70px 4px;
+        }
+        #progressBar {
+          width: 0%;
+        }
+        #sortByMarketCap:hover,
+        #sortByCurrentPrice:hover,
+        #sortByAvgDev:hover,
+        #sortByMaxDev:hover,
+        #sortByStable:hover {
+          cursor: pointer;
+          color: #0275d8;
+          transition: all 0.3s;
+        }
+      `}</style>
     </div>
   )
 }
